1bbcf8a3c30d19cf5670d894f37b8d2af623639a
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-10-17 13:37:59 +0200

    Add Basic Obfuscating

--- obfuscator.js
+++ obfuscator.js
@@ -1,13 +1,27 @@
-$('.tweet').each(function () {
-  const $tweet = $(this)
-  const userId = $tweet.find('.account-group').data('user-id') + ''
+function obfuscate ($tweets) {
+  $tweets.each(function () {
+    const $tweet = window.$(this)
+    const userId = $tweet.find('.account-group').data('user-id') + ''
 
-  const $fullName = $tweet.find('.fullname')
-  const $userName = $tweet.find('.username')
-  const $avatar = $tweet.find('.avatar')
+    const $fullName = $tweet.find('.fullname')
+    const $userName = $tweet.find('.username')
+    const $avatar = $tweet.find('.avatar')
+    const $retweetText = $tweet.find('.js-retweet-text a')
 
-  let avatarString = new window.Identicon(userId).toString()
-  $avatar.attr('src', 'data:image/png;base64,' + avatarString)
-  $fullName.text(userId)
-  $userName.text('')
+    let avatarString = new window.Identicon(userId).toString()
+    $avatar.attr('src', 'data:image/png;base64,' + avatarString)
+    $retweetText.find('b').text($retweetText.data('user-id') + ' Retweeted')
+    $fullName.text(userId)
+    $userName.text('')
+  })
+}
+
+obfuscate(window.$('.tweet'))
+
+const observer = new window.MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    obfuscate(window.$(mutation.addedNodes).find('.tweet'))
+  })
 })
+
+observer.observe(document.getElementById('stream-items-id'), { childList: true })
